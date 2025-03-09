3800003d9431773be63e38d47f1264a005b2d1d4
ArthurMazaudier <114420999+ArthurMazaudier@users.noreply.github.com>
2025-03-09 20:54:26 +0100

    Add files via upload

---
+++ EX 1 documentation/task1.sql
@@ -0,0 +1,12 @@
+-- Task 1
+/* 
+   Query: Retrieve all student records
+   Description: This query selects all columns from the "students" table, 
+   returning details such as ID, name, email, and enrollment date.
+*/
+SELECT * FROM students;
+/* Task 1
+  SELECT --> retrieves data from a table
+  * --> retrieves all columns from the table
+  FROM students --> specifies the table from which to retrieve the data
+*/
